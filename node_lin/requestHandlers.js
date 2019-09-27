a9a51496ca747d7f26bc6eb29752476a7f54107e
swq <352328759@qq.com>
2019-09-27 10:38:46 +0800

    ok

--- node_lin/requestHandlers.js
+++ node_lin/requestHandlers.js
@@ -1,9 +1,10 @@
 var querystring = require("querystring"),
-	fs = require("fs");
+	fs = require("fs"),
+	formidable = require("formidable");
 
-function start(response, postData) {
+function start(response) {
 	console.log("Request handler 'start' was called.");
-	var body = '<html>' + '<head>' + '<meta http-equiv="Content-Type" ' + 'content="text/html; charset=UTF-8" />' + '</head>' + '<body>' + '<form action="/upload" enctype="multipart/form-data" ' + 'method="post">' + '<input type="file" name="upload">' + '<input type="submit" value="Upload file" />' + '</form>' + '</body>' + '</html>';
+	var body = '<html>' + '<head>' + '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />' + '</head>' + '<body>' + '<form action="/upload" enctype="multipart/form-data" ' + 'method="post">' + '<input type="file" name="upload" multiple="multiple">' + '<input type="submit" value="Upload file" />' + '</form>' + '</body>' + '</html>';
 
 	response.writeHead(200, {
 		"Content-Type": "text/html"
@@ -12,18 +13,30 @@ function start(response, postData) {
 	response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
 	console.log("Request handler 'upload' was called.");
-	response.writeHead(200, {
-		"Content-Type": "text/plain"
+	var form = new formidable.IncomingForm();
+	console.log("about to parse");
+	
+	//设置根目录是重要的一步
+	form.uploadDir = "./tmp";
+	
+	form.parse(request, function(error, fields, files) {
+		console.log("parsing done");
+		fs.renameSync(files.upload.path, "./tmp/test.png");
+		response.writeHead(200, {
+			"Content-Type": "text/html"
+		});
+		response.write("received image:<br/>");
+		response.write("<img src='/show' />");
+		response.end();
 	});
-	response.write("You've sent the text: " + querystring.parse(postData).text);
-	response.end();
 }
 
-function show(response, postData) {
+function show(response) {
 	console.log("Request handler 'show' was called.");
-	fs.readFile("/SWQ/pit/node_lin/tmp/test.png", "binary", function(error, file) {
+//	fs.readFile("/SWQ/pit/node_lin/tmp/test.png", "binary", function(error, file) {
+	fs.readFile("./tmp/test.png", "binary", function(error, file) {
 		if(error) {
 			response.writeHead(500, {
 				"Content-Type": "text/plain"
