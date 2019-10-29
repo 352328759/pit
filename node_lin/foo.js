15d6740faed103987027d5cf0d6cb27babaa18f0
G480 <352328759@qq.com>
2019-10-30 02:21:07 +0800

    笔记

--- node_lin/foo.js
+++ node_lin/foo.js
@@ -1,9 +1,13 @@
+module.paths.push("D:\\node\\node_global\\node_modules")
 var _ = require('underscore');
+var express = require('express')
+//var routes = require('./routes')
+
+console.log(module.paths)
+console.log(express)
+
+//_.each([1, 2, 3], function (num) {
+//  console.log("underscore.js says " + num);
+//});
 
 
-_.each([1, 2, 3], function (num) {
-  console.log("underscore.js says " + num);
-});
-
-
-//1
