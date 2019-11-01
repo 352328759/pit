c75ed3c5536d0abc5451072286c892a1884e4364
swq <352328759@qq.com>
2019-11-01 18:01:05 +0800

    1

--- node_lin/foo.js
+++ node_lin/foo.js
@@ -1,10 +1,11 @@
 // module.paths.push("D:\\node\\node_global\\node_modules")
+module.paths.push("C:\\Users\\0200048\\AppData\\Roaming\\npm\\node_modules")
 var _ = require('underscore');
-// var express = require('express')
+var express = require('express')
 //var routes = require('./routes')
 
 console.log(module.paths)
-// console.log(express)
+console.log(express)
 
 //_.each([1, 2, 3], function (num) {
 //  console.log("underscore.js says " + num);
