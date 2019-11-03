372a3a0152750d29a19603ded5eeeb1edb9bf88b
SWQ <352328759@qq.com>
2019-11-03 17:36:29 +0800

    来自SWQ的修改

--- node_lin/foo.js
+++ node_lin/foo.js
@@ -5,7 +5,7 @@ var express = require('express')
 //var routes = require('./routes')
 
 console.log(module.paths)
-console.log(express)
+console.log(express) 
 
 //_.each([1, 2, 3], function (num) {
 //  console.log("underscore.js says " + num);
