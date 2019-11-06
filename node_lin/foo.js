dfcb53cd113e7327972049cea403a713e22e9031
swq <352328759@qq.com>
2019-11-06 14:58:43 +0800

    1

--- node_lin/foo.js
+++ node_lin/foo.js
@@ -1,12 +1,12 @@
 // module.paths.push("D:\\node\\node_global\\node_modules")
-module.paths.push("C:\\Users\\0200048\\AppData\\Roaming\\npm\\node_modules")
-var _ = require('underscore');
-var express = require('express')
-//var routes = require('./routes')
+// module.paths.push("C:\\Users\\0200048\\AppData\\Roaming\\npm\\node_modules")
+// console.log(module.paths)
+// console.log("module.paths 的路径可以在代码中 push 添加")
 
-console.log(module.paths)
-console.log(express)
+// var _ = require('underscore');
+// var express = require('express');
+var fs = require('fs');
 
-//_.each([1, 2, 3], function (num) {
-//  console.log("underscore.js says " + num);
-//});
+var something = process.env
+console.log(something)
+
