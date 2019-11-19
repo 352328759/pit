7952270615a62a0c98edba798bcb93a241275c6a
swq <352328759@qq.com>
2019-11-19 17:59:37 +0800

    p174 12.6

--- node_lin/foo.js
+++ node_lin/foo.js
@@ -5,8 +5,35 @@
 
 // var _ = require('underscore');
 // var express = require('express');
+// var mongoose = require('mongoose');
 // var fs = require('fs');
-//var mongoose = require('mongoose');
-var assert = require('assert');
+// var assert = require('assert');
 
-//https://www.liaoxuefeng.com/wiki/1022910821149312/1101756368943712
+var http = require('http');
+var fs = require('fs');
+var count = 0;
+
+var server = http.createServer(function (req, res) {
+    fs.readFile('./index.html', function (error, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data, 'utf-8');
+    });
+}).listen(3000, "127.0.0.1");
+console.log('Server running at http://127.0.0.1:3000/');
+
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function (socket) {
+    count++;
+    console.log('User connected. ' + count + ' user(s) present.');
+    socket.emit('users', { number: count });
+    socket.broadcast.emit('users', { number: count });
+    socket.on('disconnect', function () {
+        count--;
+        console.log('User disconnected. ' + count + ' user(s) present.');
+        socket.broadcast.emit('users', { number: count });
+    });
+});
+
+
+
