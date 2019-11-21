fa5f2bcb3fb1d10e348764b9551166a3e9089461
swq <352328759@qq.com>
2019-11-21 18:02:35 +0800

    1

--- node_lin/foo.js
+++ node_lin/foo.js
@@ -1,45 +1,40 @@
-// module.paths.push("D:\\node\\node_global\\node_modules")
-// module.paths.push("C:\\Users\\0200048\\AppData\\Roaming\\npm\\node_modules")
-// console.log(module.paths)
-// console.log("module.paths 的路径可以在代码中 push 添加")
-
 // var _ = require('underscore');
 // var express = require('express');
 // var mongoose = require('mongoose');
 // var fs = require('fs');
 // var assert = require('assert');
+// var http = require('http');
+// var io = require('socket.io');
 
-var http = require('http');
-var fs = require('fs');
 
-var server = http.createServer(function (req, res) {
-    fs.readFile('./index.html', function (error, data) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data, 'utf-8');
-    });
-}).listen(3000, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:3000/');
 
+var express = require('express');
+// var errorhandler = require('errorhandler')();
+// https://stackoverflow.com/questions/34274928/express-errorhandler
+var app = express();
+var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
-io.sockets.on('connection', function (socket) {
-    socket.on('ping1', function (data) {
-        console.log('Received PING. Sending PONG..');
-        socket.emit('pong', { text: 'PONG' });
-    });
-    socket.on('pong', function (data) {
-        console.log('Received PONG response. PONG!');
-    });
-    socket.on('message', function (data) {
-        socket.broadcast.emit('push message', data);
-        console.log("message")
-        socket.emit('push message', data);
-    });
-    setInterval(function () {
-        console.log('Sending PING to client..');
-        socket.emit('ping', { text: 'PING' });
-    }, 10000);
+app.set('port', process.env.PORT || 3000);
+
+if ('development' === app.get('env')) {
+    console.log(1)
+    app.use(express.errorHandler());
+}
+
+return false
+
+
+
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/index.html');
 });
 
 
+io.sockets.on('connection', function (socket) {
+    socket.emit('welcome', { text: 'OH HAI! U R CONNECTED!' });
+});
 
+server.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
