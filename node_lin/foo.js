2e89495154ca803ac0f35eeffb308e8b9783f2bc
swq <352328759@qq.com>
2019-11-22 17:53:14 +0800

    p196 13.2.4

--- node_lin/foo.js
+++ node_lin/foo.js
@@ -6,35 +6,48 @@
 // var http = require('http');
 // var io = require('socket.io');
 
-
-
 var express = require('express');
-// var errorhandler = require('errorhandler')();
-// https://stackoverflow.com/questions/34274928/express-errorhandler
+var errorhandler = require('errorhandler')();
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+var nicknames = [];
 
 app.set('port', process.env.PORT || 3000);
 
-if ('development' === app.get('env')) {
-    console.log(1)
-    app.use(express.errorHandler());
+if('development' === app.get('env')) {
+	app.use(errorhandler);
 }
 
-return false
-
-
-
-app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+	res.sendfile(__dirname + '/index.html');
 });
 
+io.sockets.on('connection', function(socket) {
 
-io.sockets.on('connection', function (socket) {
-    socket.emit('welcome', { text: 'OH HAI! U R CONNECTED!' });
+	socket.on('nickname', function(data, callback) {
+		if(nicknames.indexOf(data) !== -1) {
+			callback(false);
+		} else {
+			callback(true);
+			nicknames.push(data);
+			socket.nickname = data;
+			console.log('Nicknames are ' + nicknames);
+		}
+	});
+
+	socket.on('disconnect', function() {
+		if(!socket.nickname) {
+			return;
+		}
+		if(nicknames.indexOf(socket.nickname) > -1) {
+			nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+		}
+		console.log('Nicknames are ' + nicknames);
+	});
+
 });
 
-server.listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
+server.listen(app.get('port'), function() {
+	console.log('Express server listening on port ' + app.get('port'));
 });
