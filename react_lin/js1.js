e7e77026fc40c442a480c22b252fc7cbf261c59a
SWQ <352328759@qq.com>
2020-01-10 01:14:33 +0800

    lin

---
+++ react_lin/js1.js
@@ -0,0 +1,15 @@
+var name = 'Josh Perez';
+
+class Game extends React.Component {
+
+	render() {
+		return (<a href="https://react.docschina.org/docs/handling-events.html">Hello, {name} {this.props.swq}</a>)
+	}
+}
+
+// ========================================
+
+ReactDOM.render(
+	<Game swq={"s"}></Game>,
+	document.getElementById('root')
+)
