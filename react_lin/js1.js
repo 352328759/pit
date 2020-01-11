5c8766b396401d3675be4ac436212d86208ea57a
swq <352328759@qq.com>
2020-01-11 17:59:42 +0800

    react

--- react_lin/js1.js
+++ react_lin/js1.js
@@ -1,16 +1,38 @@
-var name = 'Josh Perez';
+class EssayForm extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: '请撰写一篇关于你喜欢的 DOM 元素的文章.'
+		};
 
-class Game extends React.Component {
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(event) {
+		this.setState({ value: event.target.value });
+	}
+
+	handleSubmit(event) {
+		alert('提交的文章: ' + this.state.value);
+		event.preventDefault();
+	}
 
 	render() {
-		return (<a href="https://react.docschina.org/docs/conditional-rendering.html">Hello, {name} {this.props.swq}</a>)
+		return (
+			<form onSubmit={this.handleSubmit}>
+			<a href="https://react.docschina.org/docs/lifting-state-up.html">https://react.docschina.org/docs/lifting-state-up.html</a>
+				<label>
+					文章:
+			<textarea value={this.state.value} onChange={this.handleChange} />
+				</label>
+				<input type="submit" value="提交" />
+			</form>
+		);
 	}
 }
 
-
-// ========================================
-
 ReactDOM.render(
-	<Game swq={"s"}></Game>,
+	<EssayForm></EssayForm>,
 	document.getElementById('root')
-)
+);
