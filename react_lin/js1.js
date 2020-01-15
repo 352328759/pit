7a9429315c681b4189d35c0fabcbdbc4e50fcb6c
swq <352328759@qq.com>
2020-01-15 18:02:04 +0800

    1

--- react_lin/js1.js
+++ react_lin/js1.js
@@ -1,26 +1,10 @@
-function FancyBorder(props) {
-	return (
-		<div className={'FancyBorder FancyBorder-' + props.color}>
-			{props.children}
-			<a href="https://react.docschina.org/docs/thinking-in-react.html">第四步：确定 state 放置的位置</a>
-		</div>
-	);
-}
+鼠标和指针事件
 
-function WelcomeDialog() {
-	return (
-		<FancyBorder color="blue">0
-			<h1 className="Dialog-title">
-				Welcome
-		</h1>
-			<p className="Dialog-message">
-				Thank you for visiting our spacecraft!
-		</p>
-		</FancyBorder>
-	);
-}
+https://react.docschina.org/docs/accessibility.html
+
+React.createRef()
 
 ReactDOM.render(
-	<WelcomeDialog></WelcomeDialog>,
+	<OuterClickExample />,
 	document.getElementById('root')
 );
