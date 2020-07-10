dea2e97a78e8be04e1567228d0f2f9dd638e8c95
swq <352328759@qq.com>
2020-07-10 17:57:58 +0800

    217

---
+++ react_webpack_lin/src1/index.js
@@ -0,0 +1,20 @@
+// npm install redux
+// npm install react-icons
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import storeFactory from './store'
+
+const store = storeFactory()
+
+window.React = React
+window.store = store
+
+const render = () =>
+    ReactDOM.render(
+        <App store={store} />,
+        document.getElementById('react-container')
+    )
+
+store.subscribe(render)
+render()
