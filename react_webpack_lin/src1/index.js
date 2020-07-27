64cc126ec6c7633bacf4cae9730ca723ab18ca57
swq <352328759@qq.com>
2020-07-27 17:51:07 +0800

    company-website

--- react_webpack_lin/src1/index.js
+++ react_webpack_lin/src1/index.js
@@ -1,17 +1,53 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import App from './components/App'
-import storeFactory from './store'
 
-const store = storeFactory()
+import {
+	HashRouter,
+	Route,
+	Switch,
+	Redirect,
+	BrowserRouter as Router
+} from 'react-router-dom'
+
+import {
+	Home,
+	About,
+	Events,
+	Products,
+	Contact,
+	Whoops404
+} from './pages'
 
 window.React = React
-window.store = store
 
+// render(
+// 	<Router>
+// 		<div className="main">
+// 			<Route exact path="/" component={Home} />
+// 			<Route path="/about" component={About} />
+// 			<Route path="/events" component={Events} />
+// 			<Route path="/products" component={Products} />
+// 			<Route path="/contact" component={Contact} />
+// 			<Route component={Whoops404} />
+// 		</div>
+// 	</Router>,
+// 	document.getElementById('react-container')
+// )
 render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
+	<HashRouter>
+		<div className="main">
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Redirect from="/history" to="/about/history" />
+				<Redirect from="/services" to="/about/services" />
+				<Redirect from="/location" to="/about/location" />
+				<Route path="/about" component={About} />
+				<Route path="/events" component={Events} />
+				<Route path="/products" component={Products} />
+				<Route path="/contact" component={Contact} />
+				<Route component={Whoops404} />
+			</Switch>
+		</div>
+	</HashRouter>,
 	document.getElementById('react-container')
 )
