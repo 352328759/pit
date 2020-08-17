dfeb6ff17afcdfe8e45997eccef0ccbaa09c48cc
swq <352328759@qq.com>
2020-08-17 17:56:09 +0800

    antd

--- react_webpack_lin/src1/index.js
+++ react_webpack_lin/src1/index.js
@@ -1,20 +1,31 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { HashRouter } from 'react-router-dom'
-import App from './components/App'
-import storeFactory from './store'
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+import { ConfigProvider, DatePicker, message } from 'antd';
+// 由于 antd 组件的默认文案是英文，所以需要修改为中文
+import zhCN from 'antd/es/locale/zh_CN';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+import 'antd/dist/antd.css';
+// import './index.css';
 
-const store = storeFactory()
+moment.locale('zh-cn');
 
-window.React = React
-window.store = store
+const App = () => {
+	const [date, setDate] = useState(null);
+	const handleChange = value => {
+		message.info(`您选择的日期是: ${value ? value.format('YYYY年MM月DD日') : '未选择'}`);
+		setDate(value);
+	};
+	return (
+		<ConfigProvider locale={zhCN}>
+			<div style={{ width: 400, margin: '100px auto' }}>
+				<DatePicker onChange={handleChange} />
+				<div style={{ marginTop: 16 }}>
+					当前日期：{date ? date.format('YYYY年MM月DD日') : '未选择'}
+				</div>
+			</div>
+		</ConfigProvider>
+	);
+};
 
-render(
-	<Provider store={store}>
-		<HashRouter>
-			<App />
-		</HashRouter>
-	</Provider>,
-	document.getElementById('react-container')
-)
+render(<App />, document.getElementById('react-container'));
