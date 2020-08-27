0782e6ba06444c288c781ba2d40612df2ec1f807
swq <352328759@qq.com>
2020-08-27 18:07:36 +0800

    1

--- react_webpack_lin/src1/index.js
+++ react_webpack_lin/src1/index.js
@@ -1,19 +1,54 @@
 import React, {
 	useState
-} from 'react';
-import { render } from 'react-dom';
-import { Affix, Button } from 'antd';
-
-
+} from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { HashRouter } from "react-router-dom";
+import { Affix, Button } from "antd";
 
 // 由于 antd 组件的默认文案是英文，所以需要修改为中文
-import zhCN from 'antd/es/locale/zh_CN';
-import moment from 'moment';
-import 'moment/locale/zh-cn';
-import 'antd/dist/antd.css';
-import ColumnGroup from 'antd/lib/table/ColumnGroup';
-import './stylesheets/antd.css';
-moment.locale('zh-cn');
+import zhCN from "antd/es/locale/zh_CN";
+import moment from "moment";
+import "moment/locale/zh-cn";
+import "antd/dist/antd.css";
+import ColumnGroup from "antd/lib/table/ColumnGroup";
+import "./stylesheets/antd.css";
+moment.locale("zh-cn");
+
+
+
+function counter(state = 0, action) {
+	switch (action.type) {
+		case 'INCREMENT':
+			return state + 1;
+		case 'DECREMENT':
+			return state - 1;
+		default:
+			return state;
+	}
+}
+
+let store = createStore(counter);
+
+
+store.subscribe(() =>
+	console.log(store.getState())
+);
+
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'DECREMENT' });
+
+
+
+
+
+
+
+
+
+
 
 
 const App = () => {
@@ -21,7 +56,7 @@ const App = () => {
 	const click1 = str => {
 		refBpp.refs.Cpp.onSomething("onSomething")
 	}
-	
+
 	return (
 		<>
 			<Bpp ref={i => refBpp = i}></Bpp>
@@ -30,48 +65,12 @@ const App = () => {
 	);
 };
 
-const Dpp = () => {
-	const [refName, setRefName] = useState(null);
-	const [swq, setSwq] = useState("swq");
-	const click1 = str => {
-		console.log(refName)
-	}
-	return (
-		<>
-			<div ref={setRefName}>
-				目标节点
-			</div>
-			<div onClick={click1}>click1</div>
-		</>
-	);
-};
-
-// class Dpp extends React.Component {
-
-// 	constructor(props) {
-// 		super(props)
-// 		this.state = {
-// 			// str: 'This is an editable text.',
-// 		};
-// 		// this.click1 = this.click1.bind(this)
-// 	}
-
-// 	// click1(str) { };
-
-// 	render() {
-// 		return (
-// 			<>
-// 			</>
-// 		);
-// 	}
-// }
-
 class Bpp extends React.Component {
 
 	constructor(props) {
 		super(props)
 		this.state = {
-			// str: 'This is an editable text.',
+			// str: "This is an editable text.",
 		};
 		// this.onChange = this.onChange.bind(this)
 	}
@@ -111,11 +110,38 @@ class Cpp extends React.Component {
 	}
 }
 
-class NAME extends React.Component {
-	constructor(props) {/* *** */ }
-	FunctionA() {/* 调用的方法 */ }
-	render() {/* *** */ }
-}
+const Dpp = () => {
+	const [refName, setRefName] = useState(null);
+	const [swq, setSwq] = useState("swq");
+	const click1 = str => {
+		console.log(refName)
+	}
+	return (
+		<>
+			<div ref={setRefName}>
+				目标节点
+			</div>
+			<div onClick={click1}>click1</div>
+		</>
+	);
+};
+
+/* class Dpp extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			// str: "This is an editable text.",
+		};
+		// this.click1 = this.click1.bind(this)
+	}
+	// click1(str) { };
+	render() {
+		return (
+			<>
+			</>
+		);
+	}
+} */
 
 render((
 	<>
@@ -126,5 +152,5 @@ render((
 		{/* <Cpp /> */}
 		<Dpp />
 	</>),
-	document.getElementById('react-container')
+	document.getElementById("react-container")
 );
