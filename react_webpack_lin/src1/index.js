0e8f9e3a52ea6bf61248343a641ccfa665ed2c2c
swq <352328759@qq.com>
2020-09-11 17:56:16 +0800

    antd

--- react_webpack_lin/src1/index.js
+++ react_webpack_lin/src1/index.js
@@ -5,7 +5,8 @@ import { render } from "react-dom";
 import { Provider, connect } from "react-redux";
 import { combineReducers, createStore } from "redux";
 import { HashRouter } from "react-router-dom";
-import { Affix, Button } from "antd";
+import { Button } from "antd";
+// import { HomeOutlined, UserOutlined } from '@ant-design/icons';
 
 // 由于 antd 组件的默认文案是英文，所以需要修改为中文
 import zhCN from "antd/es/locale/zh_CN";
@@ -16,95 +17,94 @@ import ColumnGroup from "antd/lib/table/ColumnGroup";
 import "./stylesheets/antd.css";
 moment.locale("zh-cn");
 
+function learnRedux() {
 
+	//redux
 
-//redux
-
-
-function reducer(state = 0, action) {
-	switch (action.type) {
-		case 'INCREMENT':
-			return state + 1;
-		case 'DECREMENT':
-			return state - 1;
-		default:
-			return state;
+	function reducer(state = 0, action) {
+		switch (action.type) {
+			case 'INCREMENT':
+				return state + 1;
+			case 'DECREMENT':
+				return state - 1;
+			default:
+				return state;
+		}
 	}
-}
-function reducer2(state = 0, action) {
-	switch (action.type) {
-		case 'case2_1':
-			console.log(action.index)
-			return state + 1;
-		case 'case2_2':
-			return state - 1;
-		default:
-			return state;
+	function reducer2(state = 0, action) {
+		switch (action.type) {
+			case 'case2_1':
+				console.log(action.index)
+				return state + 1;
+			case 'case2_2':
+				return state - 1;
+			default:
+				return state;
+		}
 	}
-}
 
-// let store = createStore(reducer);
+	// let store = createStore(reducer);
 
-// store.subscribe(() =>
-// 	console.log(store.getState())
-// );
+	// store.subscribe(() =>
+	// 	console.log(store.getState())
+	// );
 
-// store.dispatch({ type: 'INCREMENT' });
-// store.dispatch({ type: 'INCREMENT' });
-// store.dispatch({ type: 'DECREMENT' });
+	// store.dispatch({ type: 'INCREMENT' });
+	// store.dispatch({ type: 'INCREMENT' });
+	// store.dispatch({ type: 'DECREMENT' });
 
-
-function visibilityFilter(state = 'SHOW_ALL', action) {
-	switch (action.type) {
-		case 'SET_VISIBILITY_FILTER':
-			return action.filter
-		default:
-			return state
+	function visibilityFilter(state = 'SHOW_ALL', action) {
+		switch (action.type) {
+			case 'SET_VISIBILITY_FILTER':
+				return action.filter
+			default:
+				return state
+		}
 	}
-}
 
-function todos(state = [], action) {
-	switch (action.type) {
-		case 'ADD_TODO':
-			return [
-				...state,
-				{
-					text: action.text,
-					completed: false
-				}
-			]
-		case 'COMPLETE_TODO':
-			return state.map((todo, index) => {
-				if (index === action.index) {
-					return Object.assign({}, todo, {
-						completed: true
-					})
-				}
-				return todo
-			})
-		default:
-			return state
+	function todos(state = [], action) {
+		switch (action.type) {
+			case 'ADD_TODO':
+				return [
+					...state,
+					{
+						text: action.text,
+						completed: false
+					}
+				]
+			case 'COMPLETE_TODO':
+				return state.map((todo, index) => {
+					if (index === action.index) {
+						return Object.assign({}, todo, {
+							completed: true
+						})
+					}
+					return todo
+				})
+			default:
+				return state
+		}
 	}
+
+	let reducers2 = combineReducers({ reducer2, reducer })
+	let reducers = combineReducers({ reducers2, visibilityFilter, todos })
+	let store = createStore(reducers)
+
+	// console.log(store.getState())
+	// store.dispatch({
+	// 	type: 'case2_1',
+	// 	index: 1
+	// })
+	// console.log(store.getState())
+
+	// console.log("%s", reducer)
+	// console.log("%s", reducers)
+	// console.log(reducer)
+	// console.log(reducers)
+
+	//redux end
 }
-
-let reducers2 = combineReducers({ reducer2, reducer })
-let reducers = combineReducers({ reducers2, visibilityFilter, todos })
-let store = createStore(reducers)
-
-// console.log(store.getState())
-// store.dispatch({
-// 	type: 'case2_1',
-// 	index: 1
-// })
-// console.log(store.getState())
-
-// console.log("%s", reducer)
-// console.log("%s", reducers)
-// console.log(reducer)
-// console.log(reducers)
-
-//redux end
-
+// learnRedux()
 
 const App = () => {
 	let refBpp;
@@ -166,89 +166,27 @@ class Cpp extends React.Component {
 }
 
 const Dpp = () => {
+	const [refName, setRefName] = useState(null);
+	const [swq, setSwq] = useState("swq");
+	const click1 = str => {
+		console.log(refName)
+	}
 	return (
-		<div>
-			<Epp filter={"SHOW_ALL1"}>
-				All
-			</Epp>
-		</div>
-	)
+		<>
+			<Button type="primary">Primary Button</Button>
+			<Button>Default Button</Button>
+			<Button type="dashed">Dashed Button</Button>
+			<br />
+			<Button type="text">Text Button</Button>
+			<Button type="link">Link Button</Button>
+		</>
+	);
 };
-
-const mapStateToProps = (state, ownProps) => {
-	console.log(ownProps.filter)
-	console.log(state.visibilityFilter)
-	return {
-		active: ownProps.filter === state.visibilityFilter
-	}
-}
-
-const setVisibilityFilter = filter => ({
-	type: 'SET_VISIBILITY_FILTER',
-	filter
-})
-
-const mapDispatchToProps = (dispatch, ownProps) => ({
-	onClick: () => {
-		console.log(ownProps.filter)
-		return dispatch(setVisibilityFilter(ownProps.filter))
-	}
-})
-
-const Link = ({ active, children, onClick }) => (
-	<button
-		onClick={onClick}
-		disabled={active}
-		style={{
-			marginLeft: '4px',
-		}}
-	>
-		{children}
-	</button>
-)
-
-const Epp = connect(
-	mapStateToProps,
-	mapDispatchToProps
-)(Link)
-
-// const Dpp = () => {
-// 	const [refName, setRefName] = useState(null);
-// 	const [swq, setSwq] = useState("swq");
-// 	const click1 = str => {
-// 		console.log(refName)
-// 	}
-// 	return (
-// 		<>
-// 			<div ref={setRefName}>
-// 				目标节点
-// 			</div>
-// 			<div onClick={click1}>click1</div>
-// 		</>
-// 	);
-// };
-
-/* class Dpp extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			// str: "This is an editable text.",
-		};
-		// this.click1 = this.click1.bind(this)
-	}
-	// click1(str) { };
-	render() {
-		return (
-			<>
-			</>
-		);
-	}
-} */
 
 render((
 	<Provider store={store}>
-		<p><a target="_blank" href="https://ant.design/components/breadcrumb-cn/">https://ant.design/components/breadcrumb-cn/</a></p>
-		<p><a target="_blank" href="https://www.redux.org.cn/docs/basics/Reducers.html">https://www.redux.org.cn/docs/basics/Reducers.html</a></p>
+		<p><a target="_blank" href="https://ant.design/components/dropdown-cn/">https://ant.design/components/dropdown-cn/</a></p>
+		<p><a target="_blank" href="https://www.redux.org.cn/docs/basics/Store.html">https://www.redux.org.cn/docs/basics/Store.html</a></p>
 		<p><a target="_blank" href="https://blog.csdn.net/Chris__wang/article/details/97390279?utm_medium=distribute.pc_relevant_t0.none-task-blog-BlogCommendFromMachineLearnPai2-1.add_param_isCf&depth_1-utm_source=distribute.pc_relevant_t0.none-task-blog-BlogCommendFromMachineLearnPai2-1.add_param_isCf">https://blog.csdn.net/Chris__wang/article/details/97390279?utm_medium=distribute.pc_relevant_t0.none-task-blog-BlogCommendFromMachineLearnPai2-1.add_param_isCf&depth_1-utm_source=distribute.pc_relevant_t0.none-task-blog-BlogCommendFromMachineLearnPai2-1.add_param_isCf</a></p>
 		<p><a target="_blank" href="https://naotu.baidu.com/file/ebb15bfab65c13e694195623af62899b">百度</a></p>
 		<p><a target="_blank" href="https://www.processon.com/diagraming/5c8a240ee4b02ce2e88e8466">processon</a></p>
