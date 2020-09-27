dfc594690e5af35fa58ece03efb32134b2cb10b4
swq <352328759@qq.com>
2020-09-27 17:56:47 +0800

    Swq.vue

---
+++ vue_cli_lin/src/components/Swq.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="hello">
+		<a href="https://cli.vuejs.org/zh/guide/plugins-and-presets.html#%E6%8F%92%E4%BB%B6">https://cli.vuejs.org/zh/guide/plugins-and-presets.html#%E6%8F%92%E4%BB%B6</a>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Swq',
+		props: {
+			msg: String
+		}
+	}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
