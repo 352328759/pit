5ccb2cc9365efe9de6ec69df3e35f1265102b92e
swq <352328759@qq.com>
2021-03-18 17:59:39 +0800

    1

--- react_webpack_lin/src1/components/ComponentApp.js
+++ react_webpack_lin/src1/components/ComponentApp.js
@@ -3,24 +3,28 @@ import React from "react";
 // 创建一个 上下文(Context) 对象
 // "light" 是默认值, 子组件不在 xxx.Provider 时生效的值
 // 父子组件一般存在不同的文件中, 把这行代码抽象到一个独立文件, 再在用到的文件中 import 进来
-const ThemeContext = React.createContext("light");
+const ContextName = React.createContext("light");
 
 // 设置别名, 在开发者工具[react developer tools]中, 以别名显示
-ThemeContext.displayName = "MyDisplayName";
+ContextName.displayName = "MyDisplayName";
 
 class ComponentApp extends React.Component {
 	constructor(props) {
 		super(props)
-		this.state = {
-			them: "dark",
+		var _this = this
+		// this.onClick = this.onClick.bind(this)
+		this.onClick = function () {
+			console.log("在 App 更新了数据")
+			const rootData = _this.state.rootData
+			rootData.them = _this.state.rootData.them == "light" ? "dark" : "light"
+			_this.setState({ rootData })
 		};
-		this.onClick = this.onClick.bind(this)
-	}
-	onClick() {
-		console.log("在 App 更新了数据")
-		this.setState({
-			them: this.state.them == "light" ? "dark" : "light"
-		})
+		this.state = {
+			rootData: {
+				them: "dark",
+				changeTheme: this.onClick
+			},
+		};
 	}
 	render() {
 		// 使用一个 Provider 来将当前的 theme 传递给以下的组件树。
@@ -28,56 +32,66 @@ class ComponentApp extends React.Component {
 		// 在这个例子中，我们将 “dark” 作为当前的值传递下去。
 		return (
 			<>
-				<div>&lt;ThemeContext.Provider&gt; 的 value 传入上下文共用的数据</div>
-				<div>被 &lt;ThemeContext.Provider&gt; 包含的子组件, 数据不用逐层传递也可以共用</div>
-				<ThemeContext.Provider value={this.state.them}>
-					<Toolbar />
-				</ThemeContext.Provider>
-				<div onClick={this.onClick}>点击这里</div>
+				<div> ContextName.Provider 的 value 传入上下文共用的数据</div>
+				<div>被 ContextName.Provider 包含的子组件, 数据不用逐层传递也可以共用</div>
+				{/* <ContextName.Provider> 的 value 传入上下文共用的数据 */}
+				{/* 被 <ContextName.Provider> 包含的子组件, 数据不用逐层传递也可以共用 */}
+				<ContextName.Provider value={this.state.rootData}>
+					<ComA />
+				</ContextName.Provider>
+				<button onClick={this.onClick}>点击这里在父组件修改值</button>
 			</>
 		);
 	}
 }
 
 // 中间的组件再也不必指明往下传递 theme 了。
-function Toolbar(props) {
+function ComA(props) {
 	// 无状态函数式组件中, 可以用 useContext 调用上下文
-	const theme = React.useContext(ThemeContext)
-	console.log(theme)
+	const rootData = React.useContext(ContextName)
+	// 如果不是在 xxx.Provider 里调用的话, 这里就会取到 createContext 时的默认值
+	console.log(rootData.them)
 	return (
 		<div>
-			<ThemedButton />
+			<div>这是 ComA 里的 React.useContext 调用: {rootData.them}</div>
 			{/* Consumer 调用上下文 */}
-			<div>Context.Consumer 中的 value 即 Context.Provider 传入的 value</div>
-			<ThemeContext.Consumer>
+			<div>xxx.Consumer 中的 value 即 Context.Provider 传入的 value</div>
+			<ContextName.Consumer>
 				{value => (
-					<div>{value}</div>
+					<div>这是 ComA 里的 xxx.Consumer 调用: {value.them}</div>
 				)}
-			</ThemeContext.Consumer>
+			</ContextName.Consumer>
+			<ComB />
 		</div>
 	);
 }
-// Toolbar.contextType = ThemeContext;
+// ComA.contextType = ContextName;
 
-class ThemedButton extends React.Component {
-	// 指定 contextType 读取当前的 theme context。
-	// React 会往上找到最近的 theme Provider，然后使用它的值。
-	// 在这个例子中，当前的 theme 值为 “dark”。
+class ComB extends React.Component {
+	// 指定 contextType 读取当前的 ContextName。
+	// React 会往上找到最近的 xxx.Provider，然后使用它的值。
 	render() {
 		return (
 			<div>
-				<button>{this.context}</button>
-				<ThemeContext.Consumer>
-					{value => (
-						<button>{value}</button>
-					)}
-				</ThemeContext.Consumer>
+				{/* this.context 的 context 是关键字 */}
+				<div>这是 ComB 里的 contextType 调用:{this.context.them}</div>
+				<ContextName.Consumer>
+					{value => {
+						console.log(value)
+						return (
+							<>
+								<div>这是 ComB 里的 xxx.Consumer 调用:{value.them}</div>
+								<button onClick={value.changeTheme}>点击这里在子组件修改值</button>
+							</>
+						)
+					}}
+				</ContextName.Consumer>
 			</div>
 		);
 	}
 }
-// 必须指定 contextType 才能使用 Context.Consumer
-ThemedButton.contextType = ThemeContext;
+// 必须指定 contextType 才能使用 this.context
+ComB.contextType = ContextName;
 
 export default ComponentApp
 
