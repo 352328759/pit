9554cb89d65427e1a01dbc584308eb3656122c96
SWQ <352328759@qq.com>
2022-04-13 17:13:40 +0800

    pycharm 项目

---
+++ python_lin/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# input("按下 enter 键退出，其他任意键显示...\n")
+# https://www.runoob.com/python/python-basic-syntax.html
+
+# https://www.bilibili.com/video/BV1qp4y1k7su?p=67&spm_id_from=pageDriver
+
+from re import S
+import sys
+
+print(sys.path)
+
+
