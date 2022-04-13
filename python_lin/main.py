0ec86bf3106e33823afc657fc6ef414af5eab860
SWQ <352328759@qq.com>
2022-04-14 00:46:39 +0800

    弩

--- python_lin/main.py
+++ python_lin/main.py
@@ -4,11 +4,10 @@
 # input("按下 enter 键退出，其他任意键显示...\n")
 # https://www.runoob.com/python/python-basic-syntax.html
 
-# https://www.bilibili.com/video/BV1qp4y1k7su?p=67&spm_id_from=pageDriver
+# https://www.bilibili.com/video/BV1qp4y1k7su?p=71&spm_id_from=pageDriver
 
-from re import S
 import sys
+import os
 
-print(sys.path)
 
-
+print(os.makedirs("mod/m.py"))
