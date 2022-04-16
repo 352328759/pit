8350ece4e7eb57ca903d97309e233d4e3dd9d671
SWQ <352328759@qq.com>
2022-04-17 03:42:26 +0800

    python

--- python_lin/main.py
+++ python_lin/main.py
@@ -4,10 +4,6 @@
 # input("按下 enter 键退出，其他任意键显示...\n")
 # https://www.runoob.com/python/python-basic-syntax.html
 
-# https://www.bilibili.com/video/BV1qp4y1k7su?p=71&spm_id_from=pageDriver
+import openpyxl
 
-import sys
-import os
-
-
-print(os.makedirs("mod/m.py"))
+openpyxl.load_workbook("1.xlsx", data_only=True)
